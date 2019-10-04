fc84a65705daef91f2a9f710901de76619a48ffd
itsurkan <itsurkan.1@gmail.com>
2019-10-04 12:09:42 +0300

    NullCoalescingAssignment

---
+++ NullCoalescingAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NullCoalescingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = null;
+            int? a = null;
+
+            numbers ??= new List<int>();
+            numbers.Add(5);
+            Console.WriteLine(string.Join(" ", numbers));  // output: 5
+
+            numbers.Add(a ??= 0);
+            Console.WriteLine(string.Join(" ", numbers));  // output: 5 0
+            Console.WriteLine(a);  // output: 0
+
+            Console.ReadKey();
+        }
+    }
+}
