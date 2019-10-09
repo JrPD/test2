1b74ac56457747d9511542f630e85c987a61320f
itsurkan <itsurkan.1@gmail.com>
2019-10-09 12:44:25 +0300

    Default Interface members

---
+++ DefaultInterfaceMembers/SampleOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace defaultInterfaceMembers
+{
+    public class SampleOrder : IOrder
+    {
+        public SampleOrder(DateTime purchase, decimal cost) =>
+            (Purchased, Cost) = (purchase, cost);
+
+        public DateTime Purchased { get; }
+
+        public decimal Cost { get; }
+    }
+}
