f835b54963bfc6e0fdfe4177d3636e528b6544e8
itsurkan <itsurkan.1@gmail.com>
2019-10-09 12:44:25 +0300

    Default Interface members

---
+++ DefaultInterfaceMembers/ICustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace defaultInterfaceMembers
+{
+    public interface ICustomer
+    {
+        IEnumerable<IOrder> PreviousOrders { get; }
+
+        DateTime DateJoined { get; }
+        DateTime? LastOrder { get; }
+        string Name { get; }
+        IDictionary<DateTime, string> Reminders { get; }
+    }
+}
---
+++ DefaultInterfaceMembers/IOrder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace defaultInterfaceMembers
+{
+    public interface IOrder
+    {
+        DateTime Purchased { get; }
+        decimal Cost { get; }
+    }
+}
