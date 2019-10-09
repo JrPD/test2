e12fea7df35ceecc94ecdaaa93ca34462cc77dc0
itsurkan <itsurkan.1@gmail.com>
2019-10-09 12:43:54 +0300

    NullableReferenceTypes

---
+++ NullableReferenceTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NullableReferenceTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person("Ivan", null, "Tsurkan");
+            Console.WriteLine(person.GetPersonNameLength);
+        }
+    }
+
+    public class Person
+    {
+        public string  FirstName  { get; set; }
+        public string? MiddleName { get; set; }
+        public string  LastName   { get; set; }
+
+        public Person(string firstName, string? middleName, string lastName) =>
+            (FirstName, MiddleName, LastName) = (firstName, middleName, lastName);
+
+        public int GetPersonNameLength => FirstName.Length + GetMiddleNameLength() + LastName.Length;
+
+        private int GetMiddleNameLength()
+        {
+            return MiddleName.Length;
+        }
+    }
+}
